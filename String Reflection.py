8dadf920190c776d5b6985580f8bb039a4d64c50
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-14 08:19:12 +0530

    Create String Reflection.py

---
+++ String Reflection.py
@@ -0,0 +1,12 @@
+"""
+Given a string S containing only uppercase English characters. The task is to find whether S is the same as its reflection in a mirror.
+
+Examples:
+
+Input: str = "AMA"
+Output: YES
+AMA is same as its reflection in the mirror.
+
+Input: str = "ZXZ"
+Output: NO
+"""
