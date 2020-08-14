5be671670ce35c6f02f389a86e20c000fa1f6891
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-14 08:53:24 +0530

    Update Valid rectangele.py

--- Valid rectangele.py
+++ Valid rectangele.py
@@ -1,13 +1,29 @@
 """
-Given four integers A, B, C and D which represents the four angles of a Quadrilateral in degrees. The task is to check whether the given quadrilateral is valid or not.
+Given four integers A, B, C and D which represents the four angles of a Quadrilateral in degrees. 
 
+The task is to check whether the given quadrilateral is valid or not.
 
+Input:
+Input contains 4 angles of quadrilateral
+
+Output:
+Print whether it is valid quadrilateral or not
 
 Examples:
 
-Input: A = 80, B = 70, C = 100, D=110
+Input:
+80 70 100 110
 Output: Valid
-Input: A = 70, B = 80, C = 130, D=60
+
+Input: 
+70 80 130 60
 Output: Invalid
 
 """
+def Valid(a, b, c, d): 
+    if (a + b + c + d == 360): 
+        return True
+      
+    return False
+a,b,c,d=map(int,input().split())
+print("Valid" if Valid(a,b,c,d) else "Invalid")
