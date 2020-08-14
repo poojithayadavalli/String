c13f0fac1272c724b4f7f1daf4ef9e88b5393c15
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-14 09:53:55 +0530

    Update Mirror characters.py

--- Mirror characters.py
+++ Mirror characters.py
@@ -1,14 +1,44 @@
 """
-Given a string and a number N, we need to mirror the characters from N-th position up to the length of the string in the alphabetical order. In mirror operation, we change ‘a’ to ‘z’, ‘b’ to ‘y’, and so on.
+Given a string A and a number N, we need to mirror the characters from N-th position up to the length of the string in the alphabetical order.
 
+In mirror operation, we change ‘a’ to ‘z’, ‘b’ to ‘y’, and so on.
+
+Input:
+First line contains integer N
+Second line contains the string A
+
+Output:
+Print the string after performing mirror operation.
 Examples:
 
-Input : N = 3
-        paradox
-Output : paizwlc
+Input : 
+3
+paradox
+Output :
+paizwlc
 We mirror characters from position 3 to end.
 
-Input : N = 6
-        pneumonia
-Output : pnefnlmrz
+Input :
+4
+pneumonia
+Output : 
+pnefnlmrz
+
 """
+def compute(st, n): 
+    reverseAlphabet = "zyxwvutsrqponmlkjihgfedcba"
+    l = len(st) 
+    answer = "" 
+    for i in range(0, n): 
+        answer = answer + st[i] 
+              
+    for i in range(n, l): 
+        answer = (answer + 
+        reverseAlphabet[ord(st[i]) - ord('a')])
+          
+    return answer
+n=int(input())
+st=input()
+print(compute(st,n-1))
+
+
