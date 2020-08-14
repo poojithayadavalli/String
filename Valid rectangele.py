2ab552e20a98075bbd5676617cc9215b78784efd
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-14 08:39:44 +0530

    Create Valid rectangele.py

---
+++ Valid rectangele.py
@@ -0,0 +1,13 @@
+"""
+Given four integers A, B, C and D which represents the four angles of a Quadrilateral in degrees. The task is to check whether the given quadrilateral is valid or not.
+
+
+
+Examples:
+
+Input: A = 80, B = 70, C = 100, D=110
+Output: Valid
+Input: A = 70, B = 80, C = 130, D=60
+Output: Invalid
+
+"""
