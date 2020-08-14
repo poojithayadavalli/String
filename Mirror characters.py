5244c53796dc38cde5acd0014927abe3b6a19c43
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-14 08:17:31 +0530

    Create Mirror characters.py

---
+++ Mirror characters.py
@@ -0,0 +1,14 @@
+"""
+Given a string and a number N, we need to mirror the characters from N-th position up to the length of the string in the alphabetical order. In mirror operation, we change ‘a’ to ‘z’, ‘b’ to ‘y’, and so on.
+
+Examples:
+
+Input : N = 3
+        paradox
+Output : paizwlc
+We mirror characters from position 3 to end.
+
+Input : N = 6
+        pneumonia
+Output : pnefnlmrz
+"""
